111f9b063b45b8a2839805368f3d4d79d50d0f1c
Kieran Reid <kieranreid2612@gmail.com>
2026-01-11 13:24:55 +0000

    Extacted content & content meta data from url

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::net::TcpStream;
 
-const VALID_HOST: &str = "http://localhost:4221";
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
@@ -12,6 +10,7 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 let response = extract_url(&mut stream);
+                println!("{}", response);
                 stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
@@ -21,12 +20,68 @@ fn main() {
     }
 }
 
+fn extract_url(stream: &mut TcpStream) -> String {
+    let mut buf = [0; 1024];
+    let bytes_read = stream.read(&mut buf).unwrap();
+
+    let request_str = String::from_utf8_lossy(&buf[..bytes_read]);
+    let parts: Vec<&str> = request_str.split("\r\n").collect();
+
+    let request = get_request(parts);
+
+    match request.target.as_str() {
+        "/" => format!("{} 200 OK\r\n\r\n", request.version),
+        "/echo" => format!(
+            "{} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            request.version,
+            request.body.len(),
+            request.body,
+        ),
+        _ => format!("{} 404 Not Found\r\n\r\n", request.version),
+    }
+}
+
+fn get_request(parts: Vec<&str>) -> HTTPRequest {
+    println!("{:?}", parts);
+
+    let request = parts.first().unwrap();
+    let headers = parts.get(1).unwrap();
+
+    let request_parts: Vec<&str> = request.split_whitespace().collect();
+    let headers_parts: Vec<&str> = headers.split_whitespace().collect();
+
+    let _method = request_parts.first().unwrap();
+
+    let target = request_parts.get(1).unwrap();
+    let target_parts: Vec<&str> = target.split('/').collect();
+
+    let target = format!("/{}", target_parts.get(1).unwrap_or(&""));
+    let body = target_parts.get(2).unwrap_or(&"").to_string();
+
+    let version = request_parts.get(2).unwrap();
+
+    let host = headers_parts.get(1).unwrap();
+
+    let headers = HTTPHeaders {
+        host: host.to_string(),
+    };
+
+    HTTPRequest {
+        method: HTTPMethod::Get,
+        target,
+        version: version.to_string(),
+        headers,
+        body,
+    }
+}
+
 #[derive(Debug)]
 struct HTTPRequest {
     method: HTTPMethod,
     target: String,
     version: String,
     headers: HTTPHeaders,
+    body: String,
 }
 
 #[derive(Debug)]
@@ -39,43 +94,4 @@ struct HTTPHeaders {
     host: String,
     // user_agent: String,
     // accept: String,
-}
-
-fn extract_url(stream: &mut TcpStream) -> String {
-    let mut buf = [0; 1024];
-    let bytes_read = stream.read(&mut buf).unwrap();
-
-    let request_str = String::from_utf8_lossy(&buf[..bytes_read]);
-    let parts: Vec<&str> = request_str.split("\r\n").collect();
-
-    let request = parts.first().unwrap();
-    let headers = parts.get(1).unwrap();
-
-    let request = get_request(request, headers);
-
-    match request.target.as_str() {
-        "/" => format!("{} 200 OK\r\n\r\n", request.version),
-        _ => format!("{} 404 Not Found\r\n\r\n", request.version),
-    }
-}
-
-fn get_request(request: &str, headers: &str) -> HTTPRequest {
-    let request_parts: Vec<&str> = request.split_whitespace().collect();
-    let headers_parts: Vec<&str> = headers.split_whitespace().collect();
-
-    let _method = request_parts.first().unwrap();
-    let target = request_parts.get(1).unwrap();
-    let version = request_parts.get(2).unwrap();
-
-    let host = headers_parts.get(1).unwrap();
-    let headers = HTTPHeaders {
-        host: host.to_string(),
-    };
-
-    HTTPRequest {
-        method: HTTPMethod::Get,
-        target: target.to_string(),
-        version: version.to_string(),
-        headers,
-    }
 }
